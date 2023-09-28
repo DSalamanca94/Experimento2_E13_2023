2414ddc32e55cd9fb0bfc12d4ea2fed26bd9df15
David salamanca <d.salamancaz@uniandes.edu.co>
2023-09-28 18:29:59 -0500

    Autorizador v2

--- Autorizador/app.py
+++ Autorizador/app.py
@@ -1,25 +1,24 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request
 import requests
 import json
 import random
 from Autorizador import create_app
 from flask_restful import Api, Resource
-from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
+from flask_jwt_extended import create_access_token,JWTManager,jwt_required
+from datetime import datetime
 
 
 app = create_app('default')
 app_context = app.app_context()
 app_context.push()
 
-app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'super-secret'
+jwt = JWTManager(app)
 api = Api(app)
-
 
 ListaUsuarios = requests.get('http://127.0.0.1:5000/usuarios').json()
 RegistroLogin = requests.get('http://127.0.0.1:5000/RegistroLogin').json()
 
-
+fecha_hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 class VistaAutorizador(Resource):
 
@@ -29,10 +28,16 @@ class VistaAutorizador(Resource):
         contrasena = request.json.get('contrasena')
         usuario = request.json.get('usuario')
 
+        registro_Login = {
+        'usuario': usuario,
+        'ip_address': ip_address,
+        'fecha_hora': fecha_hora_actual
+}
         if any(i['usuario'] == usuario and i['contrasena'] == contrasena for i in ListaUsuarios):
 
             if usuario not in [entry['usuario'] for entry in RegistroLogin]:
                 token = create_access_token(identity=usuario)
+                requests.post('http://127.0.0.1:5000/ResigroLogin', json=registro_Login)
                 return {'token': token}, 200
             else:
                 user_entry = next(entry for entry in RegistroLogin if entry['usuario'] == usuario)
@@ -40,6 +45,7 @@ class VistaAutorizador(Resource):
                 if user_entry['ip_address'] == ip_address:
                     # Si la IP coincide, entregar el token
                     token = create_access_token(identity=usuario)
+                    requests.post('http://127.0.0.1:5000/ResigroLogin', json=registro_Login)
                     return {'token': token}, 200
                 else:
                     return {'message': 'IP no autorizada'}, 401          
