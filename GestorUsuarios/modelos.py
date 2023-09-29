836fcbfe674f9956ffce434ce29b66686cffc766
Setroun <sg.moreno1543@uniandes.edu.co>
2023-09-29 00:27:10 -0500

    Se agrega el gestor de usuarios actualizado

--- GestorUsuarios/modelos.py
+++ GestorUsuarios/modelos.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
 import enum
 
 db = SQLAlchemy()
@@ -16,6 +18,14 @@ class CanalDobleAutenticacion(enum.Enum):
    TELEFONO = 1
    CORREO_ELECTRONICO = 2
 
+
+class EnumADiccionario(fields.Field):
+    def _serialize(self, value, attr, obj, **kwargs):
+        if value is None:
+            return None
+        return {"llave": value.name, "valor": value.value}
+
+
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     usuario = db.Column(db.String(128))
@@ -31,10 +41,80 @@ class LoginHistorical (db.Model):
     #datosGeolocalizacion 
     ip = db.Column(db.String(128))
     paisIp = db.Column(db.String(128))
-    cuidadIP   = db.Column(db.String(128))
+    ciudadIP   = db.Column(db.String(128))
     sistemaOperativo = db.Column(db.String(128))
     nombreEquipo = db.Column(db.String(128))
     #datosAnalisis
     tipoActividad = db.Column(db.Enum(TipoActividad))
     accion= db.Column(db.Enum(AccionDobleAutenticacion))
     ataqueIntroducido= db.Column(db.Boolean) #True si el ataque fue introducido por el usuario, False si es una actividad normal
+
+
+class UsuarioSchema(SQLAlchemyAutoSchema):
+    idUsuario= Usuario('id', many=True) 
+    Usuario= Usuario('usuario', many=True)   
+    contrasenaUsuario= Usuario('contrasena', many=True)  
+    correoElectronicoUsuario= Usuario('correoElectronico', many=True)  
+    telefonoUsuario= Usuario('telefono', many=True)  
+    canalDobleAutenticacionUsuario= Usuario('canalDobleAutenticacion', many=True)  
+
+class LoginHistoricalSchema(SQLAlchemyAutoSchema):
+    id= LoginHistorical('id', many=True)
+    idUsuario= Usuario('id', many=True)   
+    fechaLogin= LoginHistorical('fecha', many=True)   
+    ipLogin= LoginHistorical('ip', many=True)   
+    paisIpLogin= LoginHistorical('paisIP', many=True) 
+    ciudadIpLogin= LoginHistorical('ciudadIP', many=True) 
+    sistemaOperativoLogin= LoginHistorical('sistemaOperativo', many=True) 
+    nombreEquipoLogin= LoginHistorical('nombreEquipo', many=True) 
+    tipoActividadLogin= LoginHistorical('tipoActividad', many=True) 
+    accionLogin= LoginHistorical('accion', many=True) 
+    ataqueIntroducidoLogin= LoginHistorical('ataqueIntroducido', many=True) 
+    
+    class Meta:
+         model = Usuario
+         include_relationships = True
+         load_instance = True   
+
+
+            
+class TiposActividad(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nombreTipoActividad = db.Column(db.Enum(TipoActividad))
+    Usuario=db.Column(db.Integer, db.ForeignKey('usuario.id'))
+
+class TiposActividadSchema(SQLAlchemyAutoSchema):
+    nombreTipoActividad = EnumADiccionario(attribute=("nombreTipoaActividad"))
+    class Meta:
+        model = TiposActividad
+        include_relationships = True
+        load_instance = True      
+
+
+
+class AccionesDobleAutenticacion(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nombreAccionDobleAutenticacion = db.Column(db.Enum(AccionDobleAutenticacion))
+    Usuario=db.Column(db.Integer, db.ForeignKey('usuario.id'))
+
+class AccionesDobleAutenticacionSchema(SQLAlchemyAutoSchema):
+    nombreAccionDobleAutenticacion = EnumADiccionario(attribute=("nombreAccionDobleAutenticacion"))
+    class Meta:
+        model = AccionesDobleAutenticacion
+        include_relationships = True
+        load_instance = True      
+
+
+
+class CanalesDobleAutenticacion(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nombreCanalDobleAutenticacion = db.Column(db.Enum(CanalDobleAutenticacion))
+    Usuario=db.Column(db.Integer, db.ForeignKey('usuario.id'))
+
+class CanalesDobleAutenticacionSchema(SQLAlchemyAutoSchema):
+    nombreCanalDobleAutenticacion = EnumADiccionario(attribute=("nombreAccionDobleAutenticacion"))
+    class Meta:
+        model = CanalesDobleAutenticacion
+        include_relationships = True
+        load_instance = True      
+
---
+++ GestorUsuarios/vistas.py
@@ -0,0 +1,93 @@
+from pydoc import describe
+from flask import request
+from .modelos import db, LoginHistorical,  Usuario, UsuarioSchema,  LoginHistorical, TipoActividad, TiposActividad, AccionDobleAutenticacion, AccionesDobleAutenticacionSchema, CanalDobleAutenticacion, CanalesDobleAutenticacionSchema, TiposActividadSchema   
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
+usuario_schema = UsuarioSchema()
+
+
+class VistaUsuario(Resource):
+    def post(self):
+        # Obtener los datos JSON de la solicitud
+        data = request.json
+
+        # Crear un nuevo usuario
+        nuevo_usuario = Usuario(
+            idUsuario = data['id'],
+            Usuario=data['usuario'],
+            contrasenaUsuario=data['contrasena'],
+            correoElectronicoUsuario=data['correoElectronico'],
+            telefonoUsuario=data['telefono'],
+            canalDobleAutenticacion=data['canalDobleAutenticacion'],
+        )
+
+
+        # Crear canales de doble autenticación asociadas al  usuario
+        for canales_data in data['canalDobleAutenticacion']:
+            nuevo_canal = CanalesDobleAutenticacionSchema(
+                nombreCanalDobleAutenticacion=canales_data['nombreCanalDobleAutenticacion']
+            )
+            nuevo_usuario.canalDobleAutenticacion.append(nuevo_canal)
+
+        # Agregar y guardar el nuevo usuario en la base de datos
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        return usuario_schema.dump(nuevo_usuario), 201
+    
+
+class VistaLoginHistorical(Resource):
+    def post(self):
+        # Obtener los datos JSON de la solicitud
+        data = request.json
+
+        # Crear un registro de Login
+        nuevo_login = LoginHistorical(
+            id=data['id'],
+            idUsuario=data['idUsuario'],
+            fechaLogin=data['fecha'],
+            ipLogin=data['ip'],
+            passaisIpLogin=data['paisIp'],
+            ciudadIpLogin=data['ciudadIP'],
+            sistemaOperativoLogin=data['sistemaOperativo'],
+            nombreEquipoLogin=data['nombreEquipo '],
+            tipoActividadLogin=data['tipoActividad'],
+            accionLogin=data['accion'],
+            ataqueIntroducidoLogin=data['ataqueIntroducido']
+        )
+
+
+        # Crear tipos de actividades asociadas al Login
+        for tipos_data in data['tipoActividadLogin']:
+            nuevo_tipo_Actividad = TiposActividadSchema(
+                nombreCanalDobleAutenticacion=tipos_data['nombreCanalDobleAutenticacion']
+            )
+            nuevo_login.tipoActividad.append(nuevo_tipo_Actividad)
+
+
+
+        # Crear acciones asociadas al Login
+        for acciones_data in data['accionLogin']:
+            nueva_accion = AccionesDobleAutenticacionSchema(
+                nombreAccionesDobleAutenticacion=acciones_data['nombreAccionesDobleAutenticacion']
+            )
+            nuevo_login.accion.append(nueva_accion)
+
+
+        # Agregar y guardar el nuevo usuario en la base de datos
+        db.session.add(nuevo_login)
+        db.session.commit()
+        return usuario_schema.dump(nuevo_login), 201
+    
+
+       
+    def get(self):
+        # Consulta los usuarios y ordénalos por nombre en orden descendente (Agregar nombre)
+        usuarios = Usuario.query.order_by(Usuario.id.asc()).all()    
+
+        # Serializa y devuelve los resultados (regresar un solo usuario.usuario .first)
+        return [UsuarioSchema.dump(usuario) for usuario in usuarios]
+        
+
+
+
