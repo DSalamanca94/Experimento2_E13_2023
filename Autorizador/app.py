98514b37613ea15ad58d7887b0da634c3436efe9
David salamanca <d.salamancaz@uniandes.edu.co>
2023-09-28 19:42:38 -0500

    Autorizador v3

--- Autorizador/app.py
+++ Autorizador/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 import requests
+import platform
+import socket
 import json
 import random
 from Autorizador import create_app
@@ -15,39 +17,78 @@ app_context.push()
 jwt = JWTManager(app)
 api = Api(app)
 
-ListaUsuarios = requests.get('http://127.0.0.1:5000/usuarios').json()
-RegistroLogin = requests.get('http://127.0.0.1:5000/RegistroLogin').json()
-
 fecha_hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 class VistaAutorizador(Resource):
 
-    def post(self): 
-        ip_address = request.remote_addr     
+    @staticmethod
+    def obtener_info_geografica(ip):
+        url = f"https://ipinfo.io/{ip}/json"
+        response = requests.get(url)
+        data = response.json()
+        return data
+    
+    @staticmethod
+    def obtener_info_sistema():
+        sistema_operativo = platform.system()
+        nombre_equipo = socket.gethostname()
+        return {'sistema_operativo': sistema_operativo, 'nombre_equipo': nombre_equipo}
 
+    def post(self):
         contrasena = request.json.get('contrasena')
-        usuario = request.json.get('usuario')
+        usuario =  {'usuario':request.json.get('usuario')} 
 
-        registro_Login = {
-        'usuario': usuario,
-        'ip_address': ip_address,
-        'fecha_hora': fecha_hora_actual
-}
-        if any(i['usuario'] == usuario and i['contrasena'] == contrasena for i in ListaUsuarios):
+        response = requests.get('http://127.0.0.1:5000/usuario', json=usuario)
 
-            if usuario not in [entry['usuario'] for entry in RegistroLogin]:
-                token = create_access_token(identity=usuario)
-                requests.post('http://127.0.0.1:5000/ResigroLogin', json=registro_Login)
-                return {'token': token}, 200
-            else:
-                user_entry = next(entry for entry in RegistroLogin if entry['usuario'] == usuario)
+        if response.status_code == 200:
+            InfoUsuario = response.json()
+            RegistroLogin = InfoUsuario['loginHistorical']
 
-                if user_entry['ip_address'] == ip_address:
-                    # Si la IP coincide, entregar el token
+            ip_address = request.remote_addr
+            info_geografica = requests.obtener_info_geografica(ip_address)        
+            info_sistema = requests.obtener_info_sistema()
+            
+
+            registro_Login = {
+                'idUsuario':InfoUsuario['id'],
+                'fecha': fecha_hora_actual,
+                'ip': ip_address,            
+                'paisIp': info_geografica.get('country'),
+                'ciudadIp': info_geografica.get('city'),
+                'sistemaOperativo': info_sistema.get('sistema_operativo'),
+                'nombreEquipo': info_sistema.get('nombre_equipo')
+                }
+            
+            if contrasena == InfoUsuario['contrasena']:
+
+                # Si el usuario no tiene registros de login, entregar el token
+                if usuario not in [entry['usuario'] for entry in RegistroLogin]:
                     token = create_access_token(identity=usuario)
                     requests.post('http://127.0.0.1:5000/ResigroLogin', json=registro_Login)
                     return {'token': token}, 200
+                
+                # Si el usuario tiene registros de login, validar si el registro actual es igual a alguno de los registros anteriores
                 else:
-                    return {'message': 'IP no autorizada'}, 401          
+                    registroEncontrado=False
+
+                    for registro in RegistroLogin:
+                        if registro['ip'] == ip_address & registro['paisIp'] == registro_Login['paisIp'] & registro['ciudadIp'] == registro_Login['ciudadIp'] & registro['sistemaOperativo'] == registro_Login['sistemaOperativo'] & registro['nombreEquipo'] == registro_Login['nombreEquipo']:
+                            registroEncontrado = True
+                            break
+                        else:
+                            return {'message': 'Se requiere doble Autenticacion'}, 401
+                
+                    if registroEncontrado == False:
+                        return {'message': 'Se requiere doble Autenticacion'}, 401
+                    else:
+                            token = create_access_token(identity=usuario)
+                            requests.post('http://127.0.0.1:5000/ResigroLogin', json=registro_Login)
+                            return {'token': token}, 200   
+
+
+            else:
+                return {'message': 'Credenciales incorrectas'}, 401
+        else:
+            return {'message': 'Usuario no existe'}, 401          
 
 api.add_resource(VistaAutorizador, '/autorizador')
